80324f7ae1dc563e0eee91f0682397fc3c20a7fe
ZyadYhia <zyad.yhia.sw@gmail.com>
2026-07-30 14:15:35 +0300

    edit and delete dep

--- HR.EmployeeDepartment.PL/Controllers/DepartmentController.cs
+++ HR.EmployeeDepartment.PL/Controllers/DepartmentController.cs
@@ -20,13 +20,13 @@ public class DepartmentController : Controller
         return View(departments);
     }
 
-    public async Task<IActionResult> Details(int? id)
+    public async Task<IActionResult> Details(int? id, string viewName = "Details")
     {
-        if(id is null ) return BadRequest(new {statusCode = 400, message = "Id is required"});
+        if (id is null) return BadRequest(new { statusCode = 400, message = "Id is required" });
         var dpt = await _departmentReposatory.Get(id.Value);
-        if(dpt == null)
-            return NotFound(new {statusCode = 404, message = $"Department with Id:{id} not found"});
-        return View(dpt);
+        if (dpt == null)
+            return NotFound(new { statusCode = 404, message = $"Department with Id:{id} not found" });
+        return View(viewName, dpt);
     }
 
     [HttpGet]
@@ -37,7 +37,7 @@ public class DepartmentController : Controller
     [HttpPost]
     public async Task<IActionResult> Create(CreateDepartmentDto model)
     {
-        if(ModelState.IsValid)
+        if (ModelState.IsValid)
         {
             var dpt = new Department()
             {
@@ -46,12 +46,43 @@ public class DepartmentController : Controller
                 CreatedAt = model.CreatedAt
             };
             var res = await _departmentReposatory.Add(dpt);
-            if(res > 0)
+            if (res > 0)
                 return RedirectToAction(nameof(Index));
 
         }
         return View();
     }
 
-    
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id) => await Details(id, "Edit");
+
+    [HttpPost]
+    //[ValidateAntiForgeryToken] // This line is commented out, but it is recommended to use it for security against CSRF attacks. and only accept requests from the same site.
+    public async Task<IActionResult> Edit(Department model)
+    {
+        if (ModelState.IsValid)
+        {
+            var res = await _departmentReposatory.Update(model);
+            if (res > 0)
+                return RedirectToAction(nameof(Index));
+
+        }
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int? id) => await Details(id, "Delete");
+
+    public async Task<IActionResult> Delete(Department model)
+    {
+        if (ModelState.IsValid)
+        {
+            var res = await _departmentReposatory.Delete(model);
+            if (res > 0)
+                return RedirectToAction(nameof(Index));
+
+        }
+        return View();
+    }
+
 }
